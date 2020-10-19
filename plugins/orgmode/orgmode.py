b7cbaf111d6bc7f6f347f5e4bb0e18787e563356
h_oll <harold.ollivier@mines.org>
2020-10-19 08:05:30 +0200

    Nikola auto commit.
    Nikola version: 8.1.1

--- plugins/orgmode/orgmode.py
+++ plugins/orgmode/orgmode.py
@@ -34,6 +34,7 @@ from __future__ import unicode_literals
 import io
 import os
 from os.path import abspath, dirname, join
+import shlex
 import subprocess
 
 try:
@@ -90,7 +91,8 @@ class CompileOrgmode(PageCompiler):
                 req_missing(['emacs', 'org-mode'],
                             'use the orgmode compiler', python=False)
         except subprocess.CalledProcessError as e:
-            raise Exception('Cannot compile {0} -- bad org-mode configuration (return code {1})'.format(source, e.returncode))
+            raise Exception('''Cannot compile {0} -- bad org-mode configuration (return code {1})
+The command is {2}'''.format(source, e.returncode, ' '.join(shlex.quote(arg) for arg in e.cmd)))
 
     def create_post(self, path, content=None, onefile=False, is_page=False, **kw):
         """Create post file with optional metadata."""
---
+++ shell.nix
@@ -0,0 +1,26 @@
+with import <nixpkgs> { };
+
+stdenv.mkDerivation {
+  name = "std-python";
+  buildInputs = [
+    # System requirements.
+    readline
+
+    # Python requirements (enough to get a virtualenv going).
+    python38Full
+    python38Packages.virtualenv
+    python38Packages.pip
+    python38Packages.setuptools
+    # python38Packages.numpy
+    # python38Packages.pandas
+    python38Packages.scipy
+  ];
+  src = null;
+  shellHook = ''
+    # Allow the use of wheels.
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    # Augment the dynamic linker path
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${R}/lib/R/lib:${readline}/lib
+  '';
+}
